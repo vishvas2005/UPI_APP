4cb4b115b190d22251d71dee10fdd96f5c0e6ace
vishvas2005 <156111892+vishvas2005@users.noreply.github.com>
2025-10-26 10:02:45 +0530

    second

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,25 @@
-const http = require('http')
-const express = require('express')
-const cors = require('cors')
-const mainRouter = require("./routes/indexroute")
-const app = express()
-app.use(cors())
-app.use(express.json())
-
-app.use("/api" , mainRouter)
+const express = require('express');
+const path = require('path'); 
+const cors = require('cors');
+const mainRouter = require("./routes/indexroute");
+const app = express();
 
 
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api" , mainRouter);
+const buildPath = path.join(__dirname, 'client', 'build');
+
+
+app.use(express.static(buildPath));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(buildPath, 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
